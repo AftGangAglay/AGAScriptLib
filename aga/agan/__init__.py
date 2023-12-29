a62764c52b34e37337026a95c71fae9e570834f9
Th3T3chn0G1t <prs.ttg+gh@pm.me>
2023-12-29 00:57:42 +0000

    relicence to lgpl

--- aga/agan/__init__.py
+++ aga/agan/__init__.py
@@ -1,4 +1,4 @@
-# SPDX-License-Identifier: GPL-3.0-or-later
+# SPDX-License-Identifier: LGPL-3.0-or-later
 # Copyright (C) 2023 Emily "TTG" Banerjee <prs.ttg+aga@pm.me>
 
 # This just exists as a dummy to make IDEs happy - it doesn't do anything for
--- aga/xkeys.py
+++ aga/xkeys.py
@@ -1,4 +1,4 @@
-# SPDX-License-Identifier: GPL-3.0-or-later
+# SPDX-License-Identifier: LGPL-3.0-or-later
 # Copyright (C) 2023 Emily "TTG" Banerjee <prs.ttg+aga@pm.me>
 
 # Values taken from `X11/keysymdef.h'
